738146840ff06b06a6802e57ab2321f60f2ae777
jball1997 <jball16@vols.utk.edu>
2020-01-20 05:12:38 -0500

    fixed mem issues with dask

--- combine_dfs_w_dask.py
+++ combine_dfs_w_dask.py
@@ -1,5 +1,6 @@
 ## COMBINES DATAFRAMES OF PARSED REPLAY FILES ##
-import pandas as pd
+import dask.dataframe as dd
+import dask
 from tqdm import tqdm
 import os
 
@@ -31,25 +32,25 @@ elif GRANULARITY == 0:
     rootdir = '/home/zach/Files/Nas/ReplayModels/ReplayDataProcessing/RANKED_STANDARD/Replays/1400-1600/CSVs/'
 
 for root, dirs, files in os.walk(rootdir):
-    all_dfs = pd.read_csv(rootdir+"/"+files[0], low_memory=False)
-    all_dfs.drop(columns=['Unnamed: 0'], inplace=True)
+    all_dfs = dd.read_csv(rootdir+"/"+files[0], low_memory=False)
+    all_dfs = all_dfs.drop(columns=['Unnamed: 0'])
     all_dfs = fix_ot_secs(all_dfs)
     all_dfs['0_pos_x_nf'] = all_dfs['0_pos_x'].shift(-1*FRAMES_AHEAD)
     all_dfs['0_pos_y_nf'] = all_dfs['0_pos_y'].shift(-1*FRAMES_AHEAD)
     all_dfs['0_pos_z_nf'] = all_dfs['0_pos_z'].shift(-1*FRAMES_AHEAD)
-    all_dfs = all_dfs[:-1*FRAMES_AHEAD]
+    all_dfs = all_dfs.head(len(all_dfs.index)-FRAMES_AHEAD)
     for filename in tqdm(files[1:]):
         if not filename.endswith('.csv'):
             print("\n", filename, "not a csv\n")
             continue
-        piece = pd.read_csv(rootdir+"/"+filename, low_memory=False)
-        piece.drop(columns=['Unnamed: 0'], inplace=True)
+        piece = dd.read_csv(rootdir+"/"+filename, low_memory=False)
+        piece = piece.drop(columns=['Unnamed: 0'])
         piece = fix_ot_secs(piece)
         piece['0_pos_x_nf'] = piece['0_pos_x'].shift(-1*FRAMES_AHEAD)
         piece['0_pos_y_nf'] = piece['0_pos_y'].shift(-1*FRAMES_AHEAD)
         piece['0_pos_z_nf'] = piece['0_pos_z'].shift(-1*FRAMES_AHEAD)
-        piece = piece[:-1*FRAMES_AHEAD]
-        all_dfs = all_dfs.append(piece, ignore_index=True)
+        piece = piece.head(len(piece.index)-FRAMES_AHEAD)
+        all_dfs = all_dfs.append(piece, ignore_index=True, sort=False)
 
     print(len(all_dfs))
     print("WRITING...")
